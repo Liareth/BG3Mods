9f126bae9ba932ece7123e4f2d6253fb35edcd94
Liareth <liarethnwn@gmail.com>
2023-08-15 15:09:33 +0100

    Build scripts (I'm sick of ignoring them)

---
+++ build.cmd
@@ -0,0 +1,7 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/DyeDyeDye" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/BagsBagsBags" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/LiaPersonalMod" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+
