eeb903e541152a5c7d65e56b25a2f8930ae11206
Liareth <liarethnwn@gmail.com>
2023-08-15 20:39:55 +0100

    Move Thematic Phalar Aluve from personal mod to its own mod.

--- build.cmd
+++ build.cmd
@@ -4,4 +4,4 @@ SETLOCAL EnableDelayedExpansion
 D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/DyeDyeDye" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/BagsBagsBags" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/LiaPersonalMod" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
-
+D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/ThematicPhalarAluve" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
