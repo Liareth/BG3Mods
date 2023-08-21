37d30dbba4616e60540bc455be31c59226b8904e
Liareth <liarethnwn@gmail.com>
2023-08-21 20:47:19 +0100

    Update README

--- build.cmd
+++ build.cmd
@@ -6,7 +6,8 @@ REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/ThematicPhalarAluve" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/FlameOn" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/TyrsJustice" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/LiaPersonalMod" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 
-REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/LiaPersonalMod" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/.dev/GoodDoggo" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
-D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/.dev/LuaTestBed" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/.dev/LuaTestBed" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/.dev/EveryArmour" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
