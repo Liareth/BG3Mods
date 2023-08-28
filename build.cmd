91059c6cb0b65b7e5f08443061b760e14ae0afb6
Liareth <liarethnwn@gmail.com>
2023-08-28 10:55:22 +0100

    Replace Poisons with Weapon Coatings

--- build.cmd
+++ build.cmd
@@ -2,12 +2,12 @@
 SETLOCAL EnableDelayedExpansion
 
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/DyeDyeDye" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
-REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/BagsBagsBags" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/BagsBagsBags" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/ThematicPhalarAluve" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/FlameOn" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/TyrsJustice" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
-D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/LiaPersonalMod" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/LiaPersonalMod" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/.dev/GoodDoggo" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
 REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/.dev/LuaTestBed" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
-D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/.dev/EveryArmour" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
+REM D:/BG3ModPackager/bin/Debug/net7.0-windows/BG3ModPackager.exe "C:/Users/Lia/Desktop/BG3/MyMods/.dev/EveryArmour" --deploy "C:/Users/Lia/AppData/Local/Larian Studios/Baldur's Gate 3/Mods"
